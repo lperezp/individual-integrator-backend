f68def82814cf259e666756d278848297f24ab1e
Luis Eduardo <luiseperezp.10@gmail.com>
2021-04-29 19:42:25 -0500

    feat(config): add config app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChemicalElementModule } from './modules/chemical-element/chemical-element.module';
 import { BlockModule } from './modules/block/block.module';
+import { typeORMConfig } from './config/type-orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ChemicalElementModule, BlockModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    ChemicalElementModule,
+    BlockModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// import Logger for register in log
+import { Logger } from '@nestjs/common';
+
+import * as config from 'config';
+
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const CONFIG_SERVER = config.get('server');
+
+  const port = process.env.PORT || CONFIG_SERVER.port;
+
+  await app.listen(port);
+  logger.log(`Aplication listening on port ${port}`);
 }
 bootstrap();
