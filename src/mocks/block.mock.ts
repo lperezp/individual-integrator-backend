a1eb3a24ba6dd250c13f7e183d7756a12b0ef03d
Luis Eduardo <luiseperezp.10@gmail.com>
2021-05-06 20:29:14 -0500

    feat(block): add block model

--- src/mocks/block.mock.ts
+++ src/mocks/block.mock.ts
@@ -2,17 +2,25 @@ export const BONDING_TYPE_MOCK = [
   {
     id: 1,
     value: 'S',
+    description:
+      'Los elementos del bloque s (por tener sus electrones de valencia en el orbital s) son aquellos situados en los grupos 1 y 2 de la tabla periódica de los elementos. ',
   },
   {
     id: 2,
     value: 'P',
+    description:
+      'Los elementos del bloque p (por tener sus electrones de valencia en el orbital p) son aquellos situados en los grupos III-A VIII-A de la tabla periódica de los elementos. ',
   },
   {
     id: 3,
     value: 'D',
+    description:
+      'Los elementos del bloque d (por tener electrones en el orbital d) son aquellos situados en los grupos 3 a 12 de la tabla periódica de los elementos.',
   },
   {
     id: 4,
     value: 'F',
+    description:
+      'Los elementos de transición interna o elementos del bloque f (por tener sus electrones de valencia en el orbital f) son dos series, una comenzando a partir del elemento lantano y la otra a partir del actinio, y por eso a los elementos de estas series se les llama lantánidos y actínidos. Aunque en la tabla periódica de los elementos tendrían que estar después de esos dos elementos, se suelen representar separados del resto. ',
   },
 ];
